8131dee4feafada2eb85e2e3d1b76b92b11f952b
Dragan <iliya5376@mail.com>
2016-11-01 13:18:17 +0300

    Add files via upload

--- password_strength.py
+++ password_strength.py
@@ -1,67 +1,60 @@
 import re
 
 
-## Проверяет чувствительность к регистру
+# Проверяет чувствительность к регистру
 def check_case_sensitivity(string):
-	if(string.islower()):
-		return False
-	if(string.isupper()):
-		return False
-	return True
+    return (False if(string.islower() or string.isupper()) else True)
 
-## Проверяет, есть ли цифры в строке
+
+# Проверяет, есть ли цифры в строке
 def is_numerical_digits(string):
-    if(re.search('[0-9]', string) is not None):
-    	return True
-    else:
-    	return False
-## Проверяет, есть ли метасимволы в строке
+    return (True if(re.search('[0-9]', string) is not None) else False)
+
+
+# Проверяет, есть ли метасимволы в строке
 def is_metacharacters(string):
-	if(re.search('\#|\$|\@|\%|\^|\&|\?|\!', string) is not None):
-		return True
-	else:
-		return False
-## Проверяет, является ли пароль числом
+    return (True if(re.search('\#|\$|\@|\%|\^|\&|\?|\!', string) is not None) else False)
+
+
+# Проверяет, является ли пароль числом
 def is_number(string):
-	if(string.isnumeric()):
-		return True
-	else:
-		return False
-## Проверяет, является ли пароль только строкой
+    return(True if(string.isnumeric()) else False)
+
+
+# Проверяет, является ли пароль только строкой
 def is_alpha_string(string):
-	if(string.isalpha()):
-		return True
-	else:
-		return False
+    return (True if(string.isalpha()) else False)
 
-## Даёт оценку паролю
+
+# Считает оценку паролю
 def get_password_strength(password):
-	# Если пароль пустой
-	if(password is ""):
-		return None
+    # Если пароль пустой
+    if(password is ""):
+        return None
 
-	# Если пароль непустой
-	strength_value = 1
-	# Если пароль - это строка или число
-	if(is_number(password) or is_alpha_string(password)):
-		return strength_value
-		
-	if(check_case_sensitivity(password)):
-		strength_value+=3
-	if(is_numerical_digits(password)):
-		strength_value+=2
-	if(is_metacharacters(password)):
-		strength_value+=4
-	return strength_value
-    
+    # Если пароль непустой
+    strength_value = 1
+
+    # Если пароль - это строка или число
+    if(is_number(password) or is_alpha_string(password)):
+        return strength_value
+
+    if(check_case_sensitivity(password)):
+        strength_value += 3
+    if(is_numerical_digits(password)):
+        strength_value += 2
+    if(is_metacharacters(password)):
+        strength_value += 4
+    return strength_value
+
 
 def main():
-    print("Введите пароль:")
-    password = input()
+
+    password = input("Введите пароль:\n")
     if(get_password_strength(password) is not None):
         print("Оценка введённого пароля: ", get_password_strength(password))
     else:
-    	print("Введите непустой пароль")
+        print("Введите непустой пароль")
 
 if __name__ == '__main__':
     main()
